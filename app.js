d16b4ad063344573c67b1691a2b21eb9e947ed61
hemesh3348w <69111466+hemesh3348w@users.noreply.github.com>
2020-08-19 09:56:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyD19wTzK5_pDorfufvcrvQMVHhUP_gWAS4",
+  authDomain: "phishing-2e203.firebaseapp.com",
+  databaseURL: "https://phishing-2e203.firebaseio.com",
+  projectId: "phishing-2e203",
+  storageBucket: "phishing-2e203.appspot.com",
+  messagingSenderId: "228683690573",
+  appId: "1:228683690573:web:b1640cd766b4f47618e204",
+  measurementId: "G-YP7JLN8GBM"
 };
 
      
